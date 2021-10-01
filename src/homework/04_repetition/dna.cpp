b838c908624cfb702ed7ea5ef2f9609b8b6a5b7e
ruddygarmendez <ruddy.garmendez2@g.austincc.edu>
2021-09-30 19:28:45 -0500

    hw4 w test cases

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,42 @@
 //add include statements
 
+#include "dna.h"
+
 //add function(s) code here
+
+int factorial (int num)
+{
+int prod;
+prod=1;
+
+    while (num>0){
+        prod*=num;
+        num--;
+    };
+
+return prod;
+
+}
+
+
+
+int gcd (int num1, int num2){
+
+int temp;
+
+    while (num1!=num2){
+        if (num2>num1){
+            temp=num2;
+            num2=num1;
+            num1=temp;
+        }
+
+        if (num1>num2){
+            num1=num1-num2;
+
+        }
+    
+    }
+
+
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,7 +1,14 @@
 //write include statements
 
+#include<iostream>
+#include<string>
+#include"dna.h"
+
+
 //write using statements
 
+using std::cout;
+using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -10,7 +17,68 @@ DNA string and call either get gc content or get dna complement
 function and display the result. Program runs as long as 
 user enters a y or Y.
 */
+
+
+
 int main() 
 {
+	int choice;
+	int num;
+	int num1;
+	int num2;
+
+
+
+	do{
+		cout<<"\n";
+		cout<<"MAIN MENU\n";	
+		cout<<"\n";
+		cout<<"1 - Factorial\n";	
+		cout<<"2 - Greatest Common Divisor\n";
+		cout<<"3 - Exit\n";	
+		cout<<"ENTER YOUR CHOICE:\n";
+		cin>>choice;
+	
+
+		switch(choice){
+				case 1:
+					cout<<"You have chosen Factorial\n ";
+					cout<<"Please enter an integer\n";
+					cin>>num;
+					cout<<"The answer is: "<<factorial(num);
+					break;
+					
+				case 2:
+					cout<<"You have chosen Greatest Common Divisor";
+					cout<<"Pleae enter Integer 1 \n";
+					cin>>num1;
+					cout<<"Pleae enter Integer 2 \n";
+					cin>>num2;
+					cout<<"The answer is: "<<gcd(num1,num2);
+					break;
+
+				case 3:
+					char confirm;
+					cout<<"Are you sure you want to exit? (Y) or (N)\n";
+					cin>>confirm;
+
+					if (confirm=='Y'||confirm=='y'){
+					break;
+					}
+					else{
+						choice=4;
+						break;
+					}
+
+
+
+				default:
+					cout<<"Invalid option";
+					break;	
+				}
+
+	} while (choice !=3);
+
+
 	return 0;
 }
